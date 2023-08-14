d9ef331cde5f90a38e87cc50da26608cf70ed9cd
pier <pierknows@gmail.com>
2023-08-14 09:25:45 -0400

    updated db

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,13 +1,16 @@
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Factory.Models
 {
     public class Engineer
     {
         public int EngineerId { get; set; }
+        [Required(ErrorMessage = "Enter a name")]
         public string EngineerName { get; set; }
-        public string EngineerDetails { get; set; }
         public int MachineId { get; set; }
-        public Machine Machine { get; set; }
+        
+        public List<EngineerMachine> JoinEntities { get; }
     }
 }
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,14 +1,16 @@
+
 using System.Collections.Generic;
+
 
 namespace Factory.Models
 {
     public class Machine
     {
         public int MachineId { get; set; }   
-        public string MachineType { get; set; }
-        public string MachineDescription { get; set; }
-        public Engineer Engineer { get; set; }
-        
+        public int EngineerId { get; set; }
+        public string MachineName { get; set; }
+        public string ManufactureDate { get; set; }
+        public List<EngineerMachine> JoinEntities { get; }
 
     }
 }
