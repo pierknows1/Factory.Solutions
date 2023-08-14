46062d4e2b0ed08ef2497b88b2c84b6dab8b1a25
pier <pierknows@gmail.com>
2023-08-14 09:55:38 -0400

    errors on my controllers, added index

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -6,11 +6,23 @@ using Factory.Models;
 namespace Factory.Controllers
 {
     public class HomeController : Controller
-    {
+        {
+            private readonly FactoryContext _db;
+            public HomeController(FactoryContext db)
+            {
+                _db = db;
+            }
+
         [HttpGet("/")]
         public ActionResult Index()
         {
-            return View();
+            Machine[] machines = _db.Machines.ToArray();
+            Engineer[] engineers = _db.Engineers.ToArray();
+            Dictionary<string, object[]> model = new Dictionary<string, object[]>();
+            model.Add("Machines", machines);
+            model.Add("Engineers", engineers);
+
+            return View(model);
         }
     }
 }
